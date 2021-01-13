b37cc06fe873d7691dd6cbf6446293a3aa2c30ad
Benjamin <benjamin.david1@hotmail.fr>
2021-01-13 09:34:51 +0100

    Ajout des premières migrations

---
+++ database/migrations/2021_01_13_093132_create_properties_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->bigInteger('price');
+            $table->integer('surface');
+            $table->integer('rooms')->nullable();
+            $table->enum('state', ['Neuf', 'Rénovation', 'Abandonner', 'Ancien']);
+            $table->integer('constructionYear')->nullable();
+            $table->bigInteger('zip');
+            $table->string('town');
+            $table->foreignId('type_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}

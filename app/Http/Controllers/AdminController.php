78958ba4ba94a9e31c50dfaed476b71eafec0b32
Benjamin <benjamin.david1@hotmail.fr>
2021-01-13 20:52:27 +0100

    Petite modif

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,5 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    //Les pages liée a l'admisnistration
 }
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    //mention légales
+
+    //Qui sommes nous?
 }

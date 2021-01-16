a6be28ff78c005b374a1e621aa37da80cf4fc541
Benjamin <benjamin.david1@hotmail.fr>
2021-01-16 14:10:49 +0100

    Rollback dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,110 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-2xl font-bold leading-tight text-dino-500">
-            {{ __('Les nouveaux bien à vendre') }}
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="container grid grid-cols-11 grid-rows-5 py-12 mx-auto">
-        <div class="col-span-5 row-span-full">
-            <div class="flex flex-col w-full h-full max-w-xl m-auto rounded-2xl">
-                <div
-                    class="h-full overflow-hidden text-center bg-cover rounded-t lg:rounded-t-none lg:rounded-l"
-                    style="background-image: url('https://tailwindcss.com/img/card-left.jpg')"
-                    title="Woman holding a mug">
-                </div>
-                <div
-                    class="flex flex-col justify-between w-full p-4 leading-normal bg-white border-b border-l border-r rounded-b border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light lg:rounded-b-none lg:rounded-r">
-                    <div class="flex flex-col mb-8 space-y-2">
-                        <div class="text-2xl text-dino-400 font-montserrat">Ballon Saint Mars - 72290</div>
-                        <div class="text-xl text-dino-400 font-montserrat ">666 666 €</div>
-                        <div class="text-xl text-dino-400 font-montserrat ">340 m2</div>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <span
-                            class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none rounded-full text-gray-50 bg-dino-600">Enclos à Dino</span>
-                        <button
-                            class="inline-flex items-center h-8 px-5 space-x-2 text-white transition-colors duration-150 rounded-lg bg-dino-700 focus:shadow-outline hover:bg-dino-800">
-                            <span>Voir</span>
-                            <svg class="w-6 h-6" data-darkreader-inline-fill="" data-darkreader-inline-stroke=""
-                                 fill="none" stroke="currentColor"
-                                 style="--darkreader-inline-fill:none; --darkreader-inline-stroke:currentColor;"
-                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-span-5 col-start-7 row-span-2 my-2 ">
-            <div class="w-full max-w-xl lg:flex rounded-2xl">
-                <div
-                    class="flex-none h-48 overflow-hidden text-center bg-cover rounded-t lg:h-auto lg:w-48 lg:rounded-t-none lg:rounded-l"
-                    style="background-image: url('https://tailwindcss.com/img/card-left.jpg')"
-                    title="Woman holding a mug">
-                </div>
-                <div
-                    class="flex flex-col justify-between w-full p-4 leading-normal bg-white border-b border-l border-r rounded-b border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light lg:rounded-b-none lg:rounded-r">
-                    <div class="flex flex-col mb-8 space-y-2">
-                        <div class="text-2xl text-dino-400 font-montserrat">Ballon Saint Mars - 72290</div>
-                        <div class="text-xl text-dino-400 font-montserrat ">666 666 €</div>
-                        <div class="text-xl text-dino-400 font-montserrat ">340 m2</div>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <span
-                            class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none rounded-full text-gray-50 bg-dino-600">Enclos à Dino</span>
-                        <button
-                            class="inline-flex items-center h-8 px-5 space-x-2 text-white transition-colors duration-150 rounded-xl bg-dino-700 focus:shadow-outline hover:bg-dino-800">
-                            <svg class="w-6 h-6" data-darkreader-inline-fill="" data-darkreader-inline-stroke=""
-                                 fill="none" stroke="currentColor"
-                                 style="--darkreader-inline-fill:none; --darkreader-inline-stroke:currentColor;"
-                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-span-5 col-start-7 row-span-2 row-start-4">
-            <div class="w-full max-w-xl lg:flex rounded-2xl">
-                <div
-                    class="flex-none h-48 overflow-hidden text-center bg-cover rounded-t lg:h-auto lg:w-48 lg:rounded-t-none lg:rounded-l"
-                    style="background-image: url('https://tailwindcss.com/img/card-left.jpg')"
-                    title="Woman holding a mug">
-                </div>
-                <div
-                    class="flex flex-col justify-between w-full p-4 leading-normal bg-white border-b border-l border-r rounded-b border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light lg:rounded-b-none lg:rounded-r">
-                    <div class="flex flex-col mb-8 space-y-2">
-                        <div class="text-2xl text-dino-400 font-montserrat">Ballon Saint Mars - 72290</div>
-                        <div class="text-xl text-dino-400 font-montserrat ">666 666 €</div>
-                        <div class="text-xl text-dino-400 font-montserrat ">340 m2</div>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <span
-                            class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none rounded-full text-gray-50 bg-dino-600">Enclos à Dino</span>
-                        <button
-                            class="inline-flex items-center h-8 px-5 space-x-2 text-white transition-colors duration-150 rounded-lg bg-dino-700 focus:shadow-outline hover:bg-dino-800">
-                            <span>Voir</span>
-                            <svg class="w-6 h-6" data-darkreader-inline-fill="" data-darkreader-inline-stroke=""
-                                 fill="none" stroke="currentColor"
-                                 style="--darkreader-inline-fill:none; --darkreader-inline-stroke:currentColor;"
-                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-jet-welcome/>
             </div>
         </div>
     </div>

baa84ff5328dd25a9db04b369967d9b958329752
Benjamin <benjamin.david1@hotmail.fr>
2021-01-26 22:59:26 +0100

    optimize seeder size
    Samuel m'a provoquer dans une bataille de sucre sans-merci pour avoir des seeder toujours plus opti

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,21 +19,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Création des types de propriété
-        Type::create(['title' => 'Maison individuelle', 'description' => 'Une maison est un bâtiment d\'habitation, souvent de taille moyenne destiné au logement d\'une famille, ou encore plus imposante divisée en plusieurs résidences ou appartements. Une maison est, en droit civil français, un immeuble, mot qui désigne aussi couramment un édifice de plusieurs étages divisé en plusieurs appartements occupés par diverses familles. On parle alors d\'immeuble collectif. ']);
-        Type::create(['title' => 'Appartement', 'description' => 'Un appartement est une unité d’habitation, comportant un certain nombre de pièces et qui n’occupe qu’une partie d’un immeuble, situé généralement dans une ville. ']);
-        Type::create(['title' => 'Enclos à dinosaure', 'description' => 'Un enclos est un espace de terrain entouré d\'une clôture qui sert à contenir des dinosaure, à délimiter un monument commémoratif. ']);
-
-        // Création des catégories d'article
-        Category::create(['title' => 'Maison individuelle', 'description' => 'Une maison est un bâtiment d\'habitation, souvent de taille moyenne destiné au logement d\'une famille, ou encore plus imposante divisée en plusieurs résidences ou appartements. Une maison est, en droit civil français, un immeuble, mot qui désigne aussi couramment un édifice de plusieurs étages divisé en plusieurs appartements occupés par diverses familles. On parle alors d\'immeuble collectif. ']);
-        Category::create(['title' => 'Appartement', 'description' => 'Un appartement est une unité d’habitation, comportant un certain nombre de pièces et qui n’occupe qu’une partie d’un immeuble, situé généralement dans une ville. ']);
-        Category::create(['title' => 'Enclos à dinosaure', 'description' => 'Un enclos est un espace de terrain entouré d\'une clôture qui sert à contenir des dinosaure, à délimiter un monument commémoratif. ']);
-
-        // Création des articles au quelles on attache des tags
-        Post::factory(50)->hasTags(rand(1, 10))->create();
-
-        // Création des biens au quelles on attache des images
-        Property::factory(50)->hasImages(rand(1, 10))->create();
+        // Création des libellés de types et catégories de propriété.
+        $type_category = collect([
+            ['title' => 'Maison individuelle', 'description' => 'Une maison est un bâtiment d\'habitation, souvent de taille moyenne destiné au logement d\'une famille, ou encore plus imposante divisée en plusieurs résidences ou appartements. Une maison est, en droit civil français, un immeuble, mot qui désigne aussi couramment un édifice de plusieurs étages divisé en plusieurs appartements occupés par diverses familles. On parle alors d\'immeuble collectif. '],
+            ['title' => 'Appartement', 'description' => 'Un appartement est une unité d’habitation, comportant un certain nombre de pièces et qui n’occupe qu’une partie d’un immeuble, situé généralement dans une ville. '],
+            ['title' => 'Enclos à dinosaure', 'description' => 'Un enclos est un espace de terrain entouré d\'une clôture qui sert à contenir des dinosaure, à délimiter un monument commémoratif. ']
+        ]);
+        // Création des types aux quelles ont associent des biens avec des images
+        $type_category->each(fn($item, $key) => Type::factory(['title' => $item['title'], 'description' => $item['description']])->has(Property::factory(13)->hasImages(rand(1, 10)))->create());
+        // Création des catégories aux quelles ont associent des articles avec des tags
+        $type_category->each(fn($item, $key) => Category::factory(['title' => $item['title'], 'description' => $item['description']])->has(Post::factory(13)->hasTags(rand(1, 10)))->create());
 
         //Création de l'administrateur
         User::create([

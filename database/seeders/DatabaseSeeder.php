800fd5d69ed5beda8923e50c1dff0eb51baabdfd
Benjamin <benjamin.david1@hotmail.fr>
2021-01-27 13:35:14 +0100

    win the battle of magic method and method chaining

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,15 +20,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Création des libellés de types et catégories de propriété.
-        $type_category = collect([
+        // Création des types aux quelles ont associent des biens avec des images
+        // Création des catégories aux quelles ont associent des articles avec des tags
+        collect([
             ['title' => 'Maison individuelle', 'description' => 'Une maison est un bâtiment d\'habitation, souvent de taille moyenne destiné au logement d\'une famille, ou encore plus imposante divisée en plusieurs résidences ou appartements. Une maison est, en droit civil français, un immeuble, mot qui désigne aussi couramment un édifice de plusieurs étages divisé en plusieurs appartements occupés par diverses familles. On parle alors d\'immeuble collectif. '],
             ['title' => 'Appartement', 'description' => 'Un appartement est une unité d’habitation, comportant un certain nombre de pièces et qui n’occupe qu’une partie d’un immeuble, situé généralement dans une ville. '],
             ['title' => 'Enclos à dinosaure', 'description' => 'Un enclos est un espace de terrain entouré d\'une clôture qui sert à contenir des dinosaure, à délimiter un monument commémoratif. ']
-        ]);
-        // Création des types aux quelles ont associent des biens avec des images
-        $type_category->each(fn($item, $key) => Type::factory(['title' => $item['title'], 'description' => $item['description']])->has(Property::factory(13)->hasImages(rand(1, 10)))->create());
-        // Création des catégories aux quelles ont associent des articles avec des tags
-        $type_category->each(fn($item, $key) => Category::factory(['title' => $item['title'], 'description' => $item['description']])->has(Post::factory(13)->hasTags(rand(1, 10)))->create());
+        ])  ->each(fn($type) => Type::factory(['title' => $type['title'], 'description' => $type['description']])->has(Property::factory(13)->hasImages(rand(1, 10)))->create())
+            ->each(fn($category) => Category::factory(['title' => $category['title'], 'description' => $category['description']])->has(Post::factory(13)->hasTags(rand(1, 10)))->create());
 
         //Création de l'administrateur
         User::create([

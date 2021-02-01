48b3b9a28a05d555a0237c62bb9f8dcba9998baf
Benjamin <benjamin.david1@hotmail.fr>
2021-02-01 12:31:06 +0100

    wysiwyg for home page

---
+++ database/migrations/2021_02_01_094754_create_contents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            //Contient les pages où l'on veux le contenu
+            $table->enum('for', ['Home', 'Other']);
+            $table->longText('wysiwyg_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
